eafc67a62b1b13218c069e3714a3c6735e932ebc
ESHA KUMARI <eshakumarijsr2002@gmail.com>
2025-06-13 20:51:07 +0530

    update

--- src/Home.js
+++ src/Home.js
@@ -7,35 +7,42 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export const Home = () => {
   return (
-    // <Container>
-    //   <Row className='toparea'>
-    //     <Col md-6 md-12 className='toparea1'>Pinch of Yum</Col>
-    //     <Col md-6 md-12>
-    //         <ul>
-    //             <li>Home</li>
-    //             <li>About</li>
-    //             <li>Contact</li>
-    //         </ul>
-    //     </Col>
-    //   </Row>
-    // </Container>
-    
-    <header className="toparea">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6 col-12">
-                    This Site updated on : December 04, 2023
-                </div>
-                <div className="col-md-6 col-12">
-                    <a href="mailto:gpa20102rediffmail.com"><i className="fa fa-envelope"></i>&nbsp;gpa2010@rediffmail.com</a>
-                </div>
-            </div>
-        </div>
+    <header className='toparea'>
+        <Container>
+        <Row>
+            <Col md={5} className='toparea1'>Pinch of Yum</Col>
+            <Col md={7}>
+                <Navbar expand="lg" className="bg-body-tertiary">
+                    <Container>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto links">
+                                <Nav.Link href="#home" className='links1'>HOME</Nav.Link>
+                                <Nav.Link href="#about" className='links1'>ABOUT</Nav.Link>
+                                <Nav.Link href="#recipes" className='links1'>RECIPES</Nav.Link>
+                                <Nav.Link href="#start" className='links1'>START HERE</Nav.Link>
+                            </Nav>
+                            <Form className="d-flex links">
+                                <Form.Control
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                    />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </Col>
+        </Row>
+        </Container>
     </header>
+    
+    
 
   )
 }
