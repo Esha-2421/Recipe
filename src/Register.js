867284156c057ed3e1e14d5ab724ac76bbce5955
ESHA KUMARI <eshakumarijsr2002@gmail.com>
2025-06-15 10:30:39 +0530

    update

---
+++ src/Register.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+export const Register = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>Create Account</Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Register
