cc53278e106102831541d6d187624e860cefd5d2
ESHA KUMARI <eshakumarijsr2002@gmail.com>
2025-06-15 21:57:53 +0530

    update

--- src/Invoice.js
+++ src/Invoice.js
@@ -5,7 +5,11 @@ import Col from 'react-bootstrap/Col';
 // import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './Invoice.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { MDBIcon } from 'mdb-react-ui-kit';
+import Alert from 'react-bootstrap/Alert';
+import { Button } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
 
 export const Invoice = () => {
   return (
@@ -16,15 +20,72 @@ export const Invoice = () => {
                 <h3>Enter details to make your first Sale 🚀</h3>
                 <p className='para'>First sale is made in less than a minute on Vyapar</p>
                 <Form>
+                    <Row className='invoice1'>
+                        <Col md={6}>
+                            <p><MDBIcon fas icon="file-invoice" />Invoice Details :</p>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Invoice Number</Form.Label>
+                                <Form.Control type="number" placeholder="Enter invoice number" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Invoice Date</Form.Label>
+                                <Form.Control type="date" placeholder="Invoice date" />
+                            </Form.Group>
+                        </Col>
+                        <Col md={6}>
+                            <p>Bill To :</p>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Customer Name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter customer name" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row className='invoice2'>
+                        <Col>
+                            <p>Add Sample item</p>
+                        </Col>
+                    </Row>
+                    <Row className='invoice1'>
+                        <Col>
+                            <p>Invoice calculation : </p>
+                        </Col>
+                    </Row>
                     <Row>
                         <Col md={6}>
-                            <p><FontAwesomeIcon icon="fa-duotone fa-regular fa-file-invoice" /></p>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Invoice Amount</Form.Label>
+                                <Form.Control type="number" placeholder="0.00" />
+                            </Form.Group>
                         </Col>
-                        <Col md={6}></Col>
+                        <Col md={6}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Received</Form.Label>
+                                <Form.Control type="number" placeholder="0.00" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Alert variant="success">
+                                <Row>
+                                    <Col md={6}>Balance</Col>
+                                    <Col md={6} className='am'>0.00</Col>
+                                </Row>
+                            </Alert>
+    
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className='but2'>
+                            <Button type="submit" className='but3'>Create your invoice</Button>
+                        </Col>
                     </Row>
                 </Form>
             </Col>
-            <Col md={6}></Col>
+            <Col md={6} className='sample'>
+                <h5>Sample Invoice</h5><br/>
+                <Image src="https://consumercomplaintscourt.com/wp-content/uploads/IMG-20230413-WA0068.jpg" className='img'/>
+            </Col>
         </Row>
     </Container>
   )
