140ea1c7edd2dac21e4fceb85a0b7f51b5dbfce5
ESHA KUMARI <eshakumarijsr2002@gmail.com>
2025-06-15 11:08:35 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
+import { Link, Route, Routes } from 'react-router';
 import './App.css';
 import Home from './Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Register from './Register';
 
 function App() {
   return (
-    <Home></Home>
+    <div>
+      <ul>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/register">Register</Link></li>
+      </ul>
+      <Routes>
+        <Route path=" " element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/*" element={<Home/>}/>
+      </Routes>
+    </div>
   );
 }
 
